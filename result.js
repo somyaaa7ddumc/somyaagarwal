33db6f762dcdd702e9b7fb98399ad871d0218663
somyaaa7ddumc <somyaagarwal780@gmail.com>
2026-06-25 14:05:01 +0530

    Add files via upload

---
+++ result.js
@@ -0,0 +1,11 @@
+let sname, h, e, m, total, avg, result, grade;
+sname = prompt('Enter the Name');
+h = parseInt(prompt(`Enter the Marks`));
+e = parseInt(prompt(`Enter the Marks`));
+m = parseInt(prompt(`Enter the Marks`));
+total = h + e + m;
+avg = total/3;
+if(avg>= 1 && avg <= 100) {
+    result = (avg >= 60) ? "Pass" : "Fail";
+    grade = (avg >=)
+}
