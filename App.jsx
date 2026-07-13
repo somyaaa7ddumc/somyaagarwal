29ddb34e0e940f636dbec8edbf9a3c2853d768d9
somyaaa7ddumc <somyaagarwal780@gmail.com>
2026-07-14 01:18:14 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,26 +1,17 @@
-import Navbar from "./Components/Navbar";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Skills from "./Pages/Skills";
-import Projects from "./Pages/Projects";
-import Contact from "./Pages/Contact";
+import React from 'react'
+import {useState} from 'react'
 
-import { Routes, Route } from "react-router-dom";
-
-const App = () => {
-  return (
+function App(){
+const [name,setName] = useState("somya")
+  return(
     <>
-      <Navbar />
+          <h1> hello {name} </h1>
+          <input 
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)} />
+    </>  
+  )
+}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </>
-  );
-};
-
-export default App;
+export default App
